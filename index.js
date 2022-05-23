84396cf18a2b0ea3f10e7f0ece2f7be47b84aeb0
dibdas <you@example.com>
2022-05-23 21:43:26 +0530

    modified

--- index.js
+++ index.js
@@ -23,7 +23,21 @@ function a() {
 // giving name to a function and putting that to an expression
 var g = function xyz() {
     console.log("called")
+    console.log(xyz)
 }
 a()
 g()
-xyz() // Error:Uncaught ReferenceError: xyz is not defined xyz is not created in global scope but created as a local variable
+xyz() // Error:Uncaught ReferenceError: xyz is not defined 
+    //xyz is not created in global scope but created as a local variable
+
+
+// parametrs and arguments 
+function abc(params1, params2) {
+    console.log("prams")
+
+}
+// params1, params2 are known as parameters.. parameters are locals .. they are access locally
+// parameters are those which accept the values 
+// 1 & 5 are known as arguments 
+// arguments are those through  which value are being passed
+abc(1, 5)
